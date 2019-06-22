94cddcc33ef6f20fb1fa04e094df6e19790292bb
Kevin Maldonado <kevin@brightsector.com>
2019-06-22 18:01:25 -0300

    avance

--- src/leastsquares.cpp
+++ src/leastsquares.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "leastsquares.h"
 #include <pybind11/pybind11.h>
+#include <Eigen/SVD>
 
 using namespace std;
 namespace py=pybind11;
@@ -28,6 +29,21 @@ void LeastSquaresClassifier::fit(Vector x, Vector y) {
     Matrix A = buildMatrix(x);
     // printMatrix(A);
     this->coef = (A.transpose() * A).colPivHouseholderQr().solve(A.transpose() * y);
+}
+
+// TODO: podria usar ComputeThinU nomas?
+Vector LeastSquaresClassifier::calculate(Matrix A, Vector y) {
+    py::print(A.rows());
+    py::print(A.cols());
+    py::print(y.rows());
+    Eigen::BDCSVD<Matrix> svd(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Vector c = svd.matrixU().transpose() * y;
+    Vector z = Vector::Zero(A.cols());
+    auto singularValues = svd.singularValues();
+    for (int i = 0; i < svd.nonzeroSingularValues(); i++) {
+        z(i) = c(i) / singularValues(i);
+    }
+    return svd.matrixV() * z;
 }
 
 Vector LeastSquaresClassifier::predict(Vector x) {
