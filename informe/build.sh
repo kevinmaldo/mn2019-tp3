cb16421bec3847a774a78c89c3fc5d5cf287e14d
Kevin Maldonado <kevin@brightsector.com>
2019-07-11 00:06:19 -0300

    results

--- informe/build.sh
+++ informe/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-latex template.tex
-dvips template.dvi
-ps2pdf template.ps
-latex template.tex
-dvips template.dvi
-ps2pdf template.ps
+pdflatex template.tex
+#dvips template.dvi
+#ps2pdf template.ps
+pdflatex template.tex
+#dvips template.dvi
+#ps2pdf template.ps
