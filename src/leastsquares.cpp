0fe816ce8a825acc05f87f1d1591a414e376da8a
Kevin Maldonado <kevin@brightsector.com>
2019-07-11 22:11:45 -0300

    saco fit y predict de leastsquares

--- src/leastsquares.cpp
+++ src/leastsquares.cpp
@@ -10,24 +10,8 @@ namespace py=pybind11;
 LeastSquaresClassifier::LeastSquaresClassifier() {
 }
 
-void printMatrix(Matrix m) {
-    stringstream ss;
-    ss << m;
-    py::print(ss.str());
-}
-
-Matrix LeastSquaresClassifier::buildMatrix(Vector x) {
-    //Matrix A(x.rows(), 3);
-    //A << x.cwiseProduct(x), x, Vector::Ones(x.rows());
-    //return A;
-    Matrix A(x.rows(), 2);
-    A << x, Vector::Ones(x.rows());
-    return A;
-}
-
 void LeastSquaresClassifier::fit(Vector x, Vector y) {
     Matrix A = buildMatrix(x);
-    // printMatrix(A);
     this->coef = (A.transpose() * A).colPivHouseholderQr().solve(A.transpose() * y);
 }
 
@@ -41,8 +25,4 @@ Vector LeastSquaresClassifier::calculate(Matrix A, Vector b) {
         z(i) = c(i) / singularValues(i);
     }
     return svd.matrixV() * z;
-}
-
-Vector LeastSquaresClassifier::predict(Vector x) {
-    return buildMatrix(x) * this->coef;
 }
